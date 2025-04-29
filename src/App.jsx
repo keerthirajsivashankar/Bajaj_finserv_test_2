8dc1a3022901df7600566edf34b3fabcc347da31
KEERTHI S (RA2211008020128) <135982091+keerthirajsivashankar@users.noreply.github.com>
2025-04-29 08:07:26 +0000

    completed the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginForm from "./components/LoginForm";
-import DyanmicForm from "./components/DyanamicForm";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LoginForm />} />
-          <Route path="/form" element={<DyanmicForm />} />
-        </Routes>
-      </Router>
+      <LoginForm />
     </>
   );
 }
